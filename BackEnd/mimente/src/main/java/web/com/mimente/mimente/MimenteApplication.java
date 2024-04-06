c946d97668782743e9328d81e5088b92dc972d9d
pablo segura barranco <89455065+palolo25@users.noreply.github.com>
2024-04-06 15:59:34 +0200

    Preparacion de los proyectos

---
+++ BackEnd/mimente/src/main/java/web/com/mimente/mimente/MimenteApplication.java
@@ -0,0 +1,13 @@
+package web.com.mimente.mimente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MimenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MimenteApplication.class, args);
+	}
+
+}
---
+++ BackEnd/mimente/src/test/java/web/com/mimente/mimente/MimenteApplicationTests.java
@@ -0,0 +1,13 @@
+package web.com.mimente.mimente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MimenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
