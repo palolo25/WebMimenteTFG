5911a257ac68bd96df6df4896b68bb03ccd640dd
pablo segura barranco <89455065+palolo25@users.noreply.github.com>
2024-04-06 16:19:49 +0200

    Front

---
+++ FrontEnd/WebMimente/src/app/mi-componente/mi-componente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mi-componente',
+  standalone: true,
+  imports: [],
+  templateUrl: './mi-componente.component.html',
+  styleUrl: './mi-componente.component.css'
+})
+export class MiComponenteComponent {
+
+}
