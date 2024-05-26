9a6b131a1a2c35b87a0d4712d870bdd9c8e39a9e
Alfredo Hernández-R <alfre2000bui@gmail.com>
2024-05-26 20:50:02 +0200

    prueba hosting

---
+++ FrontEnd/WebMimente/.firebase/mimente-e3582/hosting/chunk-VRSB7NKI.js
@@ -0,0 +1,1 @@
+import{d as t}from"./chunk-6GU5JNHE.js";var o=t((r,e)=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}});export default o();
